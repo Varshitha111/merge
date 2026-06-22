f7256667cc0f64496df819195bcf8bbf1bf5b67c
Varshitha111 <154863550+Varshitha111@users.noreply.github.com>
2026-06-22 17:02:14 +0530

    Update login.py

--- fe/pages/login.py
+++ fe/pages/login.py
@@ -1,13 +1,8 @@
-import requests
 import streamlit as st
-# pip install streamlit-local-storage
-from streamlit_local_storage import LocalStorage
-
-ls=LocalStorage()
-
-
+import requests
 
 b_url = st.secrets["b_url"]
+
 st.subheader("Login Form")
 
 with st.form("LoginForm"):
@@ -21,31 +16,46 @@ with st.form("LoginForm"):
     l_sub_btn = st.form_submit_button("Login")
 
 if l_sub_btn:
+
     payload = {
         "email": e,
         "password": p,
         "role": r
     }
 
-    res = requests.post(
-        f"{b_url}/login",
-        json=payload
-    )
+    try:
+        res = requests.post(
+            f"{b_url}/login",
+            json=payload
+        )
 
-    data = res.json()["obj_supabase"]["data"]
+        st.write("Status Code:", res.status_code)
 
-    if len(data) == 0:
-        st.error("User not found")
-    else:
-        st.success("Login Success")
-        st.write(data)
-        user=data[0] #{}
-        st.session_state["logged_user"] = user
-        if data[0]["role"]=="recruiter":
-            st.switch_page("pages/recruiter_dashboard.py")
+        if res.status_code != 200:
+            st.error("Backend Error")
+            st.write(res.text)
+            st.stop()
+
+        response_data = res.json()
+
+        st.write("Response:", response_data)
+
+        data = response_data["obj_supabase"]["data"]
+
+        if len(data) == 0:
+            st.error("Invalid Credentials")
         else:
-            st.switch_page("pages/job_seeker_dashboard.py")
-            
-        # Example:
-        # st.session_state["logged_user"] = data[0]
-        # st.switch_page("pages/recruiter_dashboard.py")
+            user = data[0]
+
+            # Save user in session
+            st.session_state["logged_user"] = user
+
+            st.success("Login Successful")
+
+            if user["role"] == "recruiter":
+                st.switch_page("pages/recruiter_dashboard.py")
+            else:
+                st.switch_page("pages/job_seeker_dashboard.py")
+
+    except Exception as e:
+        st.error(f"Error: {e}")
